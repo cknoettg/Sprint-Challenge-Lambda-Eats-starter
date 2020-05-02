fee23f7755152379641465770039664a591a2533
cknoettg <cknoettg@yahoo.com>
2020-05-01 21:51:20 -0400

    Added App.js with Routes and Links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Form from './components/Form';
 
 const App = () => {
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="app">
+    <Router>
+      <div>
+          <button><Link to="/">Home</Link></button>
+          <Route exact path="/" />
+          <button><Link to="/pizza">Take Me To Pizza</Link></button>
+          <Route path="/pizza" component={Form} />
+      </div>
+    </Router>
+    </div>
   );
 };
+
 export default App;
